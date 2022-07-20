bc091bc75922cf833040d2eacce31bf7370c3727
xgnatyuk <105510681+xgnatyuk@users.noreply.github.com>
2022-07-20 20:11:15 +0300

    js upd

--- script.js
+++ script.js
@@ -1,9 +1,6 @@
 (function() {
-    var menuItems = document.getElementsByClassName("sidebar__item")[0];
+    var menuItems = document.getElementsByClassName("sidebar")[0];
     var menuButton = document.getElementsByClassName("menu")[0];
-
-    menuItems.classList.toggle('responsive');
-
     menuButton.addEventListener('click', function() {
         menuItems.classList.toggle('responsive');
     });
